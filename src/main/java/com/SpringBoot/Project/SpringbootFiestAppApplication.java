c8f23dd5e816c1de5dcf247201f3623c96f1ee1e
sajid786005 <tech.sajid.abbas@gmail.com>
2024-04-22 12:57:44 +0530

    first commit

---
+++ src/main/java/com/SpringBoot/Project/SpringbootFiestAppApplication.java
@@ -0,0 +1,13 @@
+package com.SpringBoot.Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootFiestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootFiestAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SpringBoot/Project/SpringbootFiestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringBoot.Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootFiestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
